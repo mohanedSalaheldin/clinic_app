fffaf10c1a4362512afa613b4444bb7d0e0bc039
mohanedSalaheldin <mohnsd1162002@gmail.com>
2026-02-02 21:18:42 +0200

    add admin layout

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -17,13 +17,18 @@ class AuthController
     public function login()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            if ($this->auth->login($_POST['email'], $_POST['password'])) {
+            $loginResult = $this->auth->login($_POST['email'], $_POST['password']);
+            var_dump($loginResult);
+            if ($loginResult=="Patiant") {
                 header("Location: index.php?route=home");
+                exit;
+            }else if ($loginResult=="Doctor") {
+                header("Location: index.php?route=contact");
                 exit;
             }
             $error = "invaild data";
         }
-        require __DIR__ . '/../../views/login.php';
+        require __DIR__ . '/../../views/front/login.php';
     }
     
     public function register()
@@ -40,12 +45,13 @@ class AuthController
 
             if ($this->auth->register($data)) {
                 header("Location: index.php?route=login");
+
                 exit;
             } else {
                 $error = "Error: Email Used before";
             }
         }
-        require __DIR__ . '/../../views/register.php';
+        require __DIR__ . '/../../views/front/register.php';
     }
 
     public function logout()
